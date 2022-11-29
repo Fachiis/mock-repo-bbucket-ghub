526e7c77a487f443f160a08ed8bc71cb7010bd81
Fachii Zasha <zashafachii@gmail.com>
2022-11-29 02:07:51 +0000

    add code in files types: .tsx,.ts remove code in files types: .tsx,.ts

--- content.ts
+++ content.ts
@@ -1,4 +1,4 @@
 console.log("bkffc")
 console.log("ynrcx")
 console.log("yhthf")
-console.log("ggwnl")
+console.log("gwihh")
--- content.tsx
+++ content.tsx
@@ -1,3 +1,3 @@
 console.log("pqhil")
 console.log("cdkil")
-console.log("vtrlc")
+console.log("vnfew")
