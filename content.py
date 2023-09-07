b5557b33a48cab022977ef94ce52339061ac7cd4
Fachii Zasha <zashafachii@gmail.com>
2023-09-07 10:38:28 +0000

    add code in files types: .py remove code in files types: .py

--- content.py
+++ content.py
@@ -14,4 +14,4 @@ importer = Importer([repo, repo2], mock_repo)
 # Since the private repo uses work email, and Github uses my personal email
 print("tbasd")
 print("mwudq")
-print("npvvy")
+print("ulrlf")
