ec87023ab0672500a833f444bedc6339b33e314b
fachiis <zashafachii@gmail.com>
2024-10-01 23:08:05 +0100

    Added poblysh repo

--- run_script.py
+++ run_script.py
@@ -7,9 +7,10 @@ from git_contributions_importer import Importer
 # Your private repo or Bitbucket repo
 repo = git.Repo("/Users/fachiizasha/Code/iCUBEFARM/icfui-next")
 repo2 = git.Repo("/Users/fachiizasha/Code/iCUBEFARM/icf_upgrade")
+repo3 = git.Repo("/Users/fachiizasha/Code/Poblysh/poblysh-frontend")
 # Your mock repo
 mock_repo = git.Repo("/Users/fachiizasha/Code/mock-repo-bbucket-ghub")
-importer = Importer([repo, repo2], mock_repo)
+importer = Importer([repo, repo2, repo3], mock_repo)
 # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github uses my personal email
 importer.set_author(['zashafachii@gmail.com', 'felix@icubefarm.com'])
