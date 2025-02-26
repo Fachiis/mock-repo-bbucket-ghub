a4f79a20b3df7c9351c74b23d4a30accfe82466a
fachiis <zashafachii@gmail.com>
2025-02-26 08:56:33 +0100

    update run_script.py to remove unused repo and simplify author settings; modify mock-repo-bbucket-ghub.iml to specify Python SDK

--- run_script.py
+++ run_script.py
@@ -8,11 +8,10 @@ from git_contributions_importer import Importer
 repo = git.Repo("/Users/fachiizasha/Code/iCUBEFARM/icfui-next")
 repo2 = git.Repo("/Users/fachiizasha/Code/iCUBEFARM/icf_upgrade")
 repo3 = git.Repo("/Users/fachiizasha/Code/Poblysh/poblysh-frontend")
-repo4 = git.Repo("/Users/fachiizasha/Code/Online-Car-Rentals/car-rent-backend")
 # Your mock repo
 mock_repo = git.Repo("/Users/fachiizasha/Code/mock-repo-bbucket-ghub")
-importer = Importer([repo, repo2, repo3, repo4], mock_repo)
+importer = Importer([repo, repo2, repo3], mock_repo)
 # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github uses my personal email
-importer.set_author(['zashafachii@gmail.com', 'felix@icubefarm.com', 'felix@poblysh.com', "felix@mmncarrentals.com"])
+importer.set_author(['zashafachii@gmail.com', 'felix@icubefarm.com', 'felix@poblysh.com',])
 importer.import_repository()
