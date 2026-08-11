8c81e76c7f108df66631374622cfd05df8b31a0e
fachiis <zashafachii@gmail.com>
2026-08-11 17:17:46 +0100

    refactor: reorganize imports and enhance repository initialization

--- run_script.py
+++ run_script.py
@@ -1,5 +1,6 @@
-import sys
 import os
+import sys
+
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -7,19 +8,21 @@ load_dotenv()
 
 # Add the contributions importer to the Python path
 sys.path.append("./Contributions-Importer-For-Github/")
-from git_contributions_importer import Importer
 import git
+from git_contributions_importer import Importer
 
 # Get repository paths from environment variables
-repo = git.Repo(os.getenv('PROJ_ONE'))
-repo2 = git.Repo(os.getenv('PROJ_TWO'))
-repo3 = git.Repo(os.getenv('PROJ_THREE'))
+repo = git.Repo(os.getenv("PROJ_ONE"))
+repo2 = git.Repo(os.getenv("PROJ_TWO"))
+repo3 = git.Repo(os.getenv("PROJ_THREE"))
+repo4 = git.Repo(os.getenv("PROJ_FOUR"))
+repo5 = git.Repo(os.getenv("PROJ_FIVE"))
 
 # Your mock repo
-mock_repo = git.Repo(os.getenv('MOCK_REPO_PATH'))
-importer = Importer([repo, repo2, repo3], mock_repo)
+mock_repo = git.Repo(os.getenv("MOCK_REPO_PATH"))
+importer = Importer([repo, repo2, repo3, repo4], mock_repo)
 
 # Get author emails from environment variables and split by comma
-author_emails = os.getenv('AUTHOR_EMAILS').split(',')
+author_emails = os.getenv("AUTHOR_EMAILS").split(",")
 importer.set_author(author_emails)
 importer.import_repository()
